e909985708b211eb3f416f5bd318c54f86b6d81b
Neelesh kumar <neeleshkumarm98@gmail.com>
2023-11-29 01:44:45 +0530

    adding BCryptPassword Encoder in Backend to secure password

--- ExamPortalServer/src/main/java/com/examPortal/ExamPortalServerApplication.java
+++ ExamPortalServer/src/main/java/com/examPortal/ExamPortalServerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.examPortal.model.Role;
 import com.examPortal.model.User;
@@ -14,39 +15,44 @@ import com.examPortal.model.UserRole;
 import com.examPortal.service.UserService;
 
 @SpringBootApplication
-public class ExamPortalServerApplication implements CommandLineRunner{
+public class ExamPortalServerApplication implements CommandLineRunner {
 
 	@Autowired
 	private UserService userService;
 	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+
 	public static void main(String[] args) {
 		SpringApplication.run(ExamPortalServerApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
+
+		// System.out.println("program started");
+
 		
-		//System.out.println("program started");
-		
-		/*
-		 * User user=new User();
-		 * 
-		 * user.setFirstname("Neelesh"); user.setLastname("Kumar");
-		 * user.setUsername("neeleshkumarm98");
-		 * user.setEmail("neeleshkumarm98@gmail.com"); user.setPassword("Neelesh@123");
-		 * user.setPhone("9471458227"); user.setImage("default.png");
-		 * 
-		 * 
-		 * Role role1=new Role(); role1.setRoleId(44L); role1.setRolename("ADMIN");
-		 * 
-		 * Set<UserRole> userRoleSet= new HashSet<>(); UserRole userRole=new UserRole();
-		 * userRole.setRole(role1); userRole.setUser(user);
-		 * 
-		 * userRoleSet.add(userRole);
-		 * 
-		 * User user1=this.userService.createUsers(user, userRoleSet);
-		 * System.out.println(user1);
-		 */	
-		}
+		  User user=new User();
+		  
+		  user.setFirstname("Neelesh"); 
+		  user.setLastname("Kumar");
+		  user.setUsername("neeleshkumarm98");
+		  user.setEmail("neeleshkumarm98@gmail.com"); 
+		  user.setPassword(this.passwordEncoder.encode("Neelesh@123"));
+		  user.setPhone("9471458227"); user.setImage("default.png");
+		  
+		  
+		  Role role1=new Role(); role1.setRoleId(44L); role1.setRolename("ADMIN");
+		  
+		  Set<UserRole> userRoleSet= new HashSet<>(); UserRole userRole=new UserRole();
+		  userRole.setRole(role1); userRole.setUser(user);
+		  
+		  userRoleSet.add(userRole);
+		  
+		  User user1=this.userService.createUsers(user, userRoleSet);
+		  System.out.println(user1);
+		 
+	}
 
 }
--- ExamPortalServer/src/main/java/com/examPortal/controller/UserController.java
+++ ExamPortalServer/src/main/java/com/examPortal/controller/UserController.java
@@ -7,6 +7,7 @@ import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,11 +30,17 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	
 	//creating user
 	@PostMapping("/")
 	public ResponseEntity<User> createUser(@RequestBody User user) throws Exception
 	{
 		Set<UserRole> roles=new HashSet<>();
+		
+		//encode password with BCryptPassword
+		user.setPassword(this.bCryptPasswordEncoder.encode(user.getPassword()));
 		
 		Role role=new Role();
 		role.setRoleId(45L);
